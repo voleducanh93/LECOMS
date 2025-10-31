8a7e8d10a4eaefd03ddf1f9c7efcbb8ab5254fb7
Duc Hau <hauphanduc3014@gmail.com>
2025-10-31 17:27:36 +0700

    Revert "Update Confirm Email"
    This reverts commit d650e07be34a343d78d89a7a1283b38f9cb83068.

--- LECOMS/LECOMS.Service/Services/AuthService.cs
+++ LECOMS/LECOMS.Service/Services/AuthService.cs
@@ -138,7 +138,7 @@ namespace LECOMS.Service.Services
 
             // Generate confirmation email token and send email
             var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            var confirmLink = $"{_configuration["AppSettings:FrontendUrl"]}/confirm-email?email={user.Email}&token={token}";
+            var confirmLink = $"https://lecom-fe.vercel.app/auth/email-confirmed?email={user.Email}&token={Uri.EscapeDataString(token)}";
             _emailService.SendEmailConfirmation(user.Email, confirmLink);
 
             return user;
