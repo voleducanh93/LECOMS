80fb93e7e32d5ce54e858284995318ac6e501ef0
Vo Le Duc Anh <voleducanh93@gmail.com>
2025-11-25 11:08:51 +0700

    RewardCode = b.Code,                   // ⭐ ADD

--- LECOMS/LECOMS.Service/Services/GamificationService.cs
+++ LECOMS/LECOMS.Service/Services/GamificationService.cs
@@ -187,7 +187,7 @@ namespace LECOMS.Service.Services
                 return new RewardItemDTO
                 {
                     Id = v.Id,
-                    RewardCode = b.Code,                   // ⭐ ADD
+                    RewardCode = v.Code,                   // ⭐ ADD
                     Title = v.Code,                         // FE field: title
                     Description = BuildVoucherDescription(v),
                     CoinCost = rule?.CostPoints ?? 0,       // FE field: coinCost
