308c3fdff184c7d5fb96be5c3e755b7055adfee5
Vo Le Duc Anh <voleducanh93@gmail.com>
2025-12-01 16:52:39 +0700

    slug

--- LECOMS/LECOMS.Data/DTOs/Product/ProductCategoryDTO.cs
+++ LECOMS/LECOMS.Data/DTOs/Product/ProductCategoryDTO.cs
@@ -11,5 +11,6 @@ namespace LECOMS.Data.DTOs.Product
         public string Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+        public string? Slug { get; set; }
     }
 }
